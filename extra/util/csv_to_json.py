287a8160ba2da9d48b8a2d06cb1325d0051158c9
stephbuon <steph.buon@gmail.com>
2022-07-08 15:05:42 -0500

    edited readme

--- extra/util/csv_to_json.py
+++ extra/util/csv_to_json.py
@@ -13,7 +13,8 @@ def csv_to_json(csvFilePath, jsonFilePath):
     with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
         jsonString = json.dumps(jsonArray, indent=4)
         jsonf.write(jsonString)
-          
+
+
 csvFilePath = r'/home/stephbuon/data/us_congress_2001.csv'
 jsonFilePath = r'/home/stephbuon/data/congress_2001.json'
 csv_to_json(csvFilePath, jsonFilePath)
